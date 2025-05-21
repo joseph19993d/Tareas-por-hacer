e8e98f0a92d1e797c9ce118954d54f1bd9ba3446
Joseph Rodelo Suarez <rodelosuaresj@gmail.com>
2025-05-21 00:59:13 -0500

    Add comments for clarity in listadoArr and cargarTareasFromArray methods

--- models/tareas.js
+++ models/tareas.js
@@ -5,6 +5,7 @@ class Tareas {
     _listado={};
 
     get listadoArr(){
+        // convertir el objeto a un array 
         const listado = [];
         Object.keys(this._listado).forEach(key=>{
             listado.push(this._listado[key]);
@@ -19,6 +20,13 @@ class Tareas {
     crearTarea(desc){
         const tarea = new Tarea(desc)
         this._listado[tarea.id] = tarea;
+    }
+
+    cargarTareasFromArray(tareas = []){
+        // Agrega las tareas al listado
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        });
     }
 
 }
