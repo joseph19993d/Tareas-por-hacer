39497398fbdebf0242247bfa75d0d97d1cc49942
Joseph Rodelo Suarez <rodelosuaresj@gmail.com>
2025-06-17 02:51:34 -0500

    Refactor app.js for improved readability and add missing functions for task management

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
-require('colors');
-const {inquirerMenu, pausa, validateInput} = require('./helpers/inquirer');
-const Tareas = require('./models/tareas'); 
-const {guardarDB, leerDB} = require('./helpers/guardarArchivo');
+require("colors");
+const {
+  inquirerMenu,
+  pausa,
+  validateInput,
+  listadoTareasBorrar,
+  confirmar,
+} = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
+const { guardarDB, leerDB } = require("./helpers/guardarArchivo");
 
-const main = async() =>{
-    let opt='';
-    const tareas = new Tareas();
+const main = async () => {
+  let opt = "";
+  const tareas = new Tareas();
 
-    const tareasDB = leerDB(); 
+  const tareasDB = leerDB();
 
-    if(tareasDB){
-        // Establecer las tareas
-        // tareas = tareasDB;
-        tareas.cargarTareasFromArray(tareasDB);
+  if (tareasDB) {
+    // Establecer las tareas
+    // tareas = tareasDB;
+    tareas.cargarTareasFromArray(tareasDB);
+  }
+
+  do {
+    opt = await inquirerMenu();
+    switch (opt) {
+      case "1":
+        const desc = await validateInput("Descripcion:");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        //listado de tareas [ARRARY]
+        console.log(tareas.listadoArr);
+        break;
+      case "3":
+        tareas.listarPendiantesCompletadas(true);
+        break;
+      case "4":
+        tareas.listarPendiantesCompletadas(false);
+        break;
+
+      case "6":
+        const id = await listadoTareasBorrar(tareas.listadoArr);
+        const ok = await confirmar("¿estas seguro?");
+        if (Boolean(ok)) {
+          tareas.borrarTarea(id);
+          console.log("Tarea borrada");
+        } 
+        break;
     }
 
-    do{
-        opt = await inquirerMenu();
-        switch(opt){
-            case '1':
-                const desc = await validateInput('Descripcion:');
-                tareas.crearTarea(desc);
-            break;
-            case '2':
-                //listado de tareas [ARRARY]
-                console.log(tareas.listadoArr);
-            break;
-        }
-        
-        guardarDB(tareas.listadoArr);
-        await pausa();
-    }while(opt != '0')
-
-}
+    guardarDB(tareas.listadoArr);
+    await pausa();
+  } while (opt != "0");
+};
 
 main();
