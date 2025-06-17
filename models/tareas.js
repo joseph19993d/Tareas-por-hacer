5c1866ba64a4320dc2ffa0599e49c8e61eee9952
Joseph Rodelo Suarez <rodelosuaresj@gmail.com>
2025-06-17 02:51:28 -0500

    Refactor Tareas class for improved structure and readability

--- models/tareas.js
+++ models/tareas.js
@@ -1,33 +1,65 @@
-const Tarea = require('./tarea')
+const Tarea = require("./tarea");
 
 class Tareas {
+  _listado = {};
 
-    _listado={};
+  get listadoArr() {
+    // convertir el objeto a un array
+    const listado = [];
+    Object.keys(this._listado).forEach((key) => {
+      listado.push(this._listado[key]);
+    });
+    return listado;
+  }
 
-    get listadoArr(){
-        // convertir el objeto a un array 
-        const listado = [];
-        Object.keys(this._listado).forEach(key=>{
-            listado.push(this._listado[key]);
-        })
-        return listado;
+  constructor() {
+    this._listado = {};
+  }
+
+  crearTarea(desc) {
+    const tarea = new Tarea(desc);
+    this._listado[tarea.id] = tarea;
+  }
+
+  cargarTareasFromArray(tareas = []) {
+    // Agrega las tareas al listado
+    tareas.forEach((tarea) => {
+      this._listado[tarea.id] = tarea;
+    });
+  }
+
+  listadoCompleto() {
+    this.listadoArr.forEach((tarea, idx) => {
+      const idxStr = `${idx + 1}.`.green;
+      const { desc, completadoEn } = tarea;
+      const estado = completadoEn ? "Completada".green : "Pendiente".red;
+      console.log(`${idxStr} ${desc} :: ${estado}`);
+    });
+  }
+
+  listarPendiantesCompletadas(completadas = true) {
+    let contador = 0;
+    for (let i = 0; i < this.listadoArr.length; i++) {
+      const tarea = this.listadoArr[i];
+      const completada = !!tarea.completadoEn;
+
+      if (completada === completadas) {
+        contador++;
+        console.log(
+          `${contador.toString().green}. ${tarea.desc} :: ${
+            completada ? "Completada".green : "Pendiente".red
+          }`
+        );
+      }
     }
+  }
 
-    constructor(){
-        this._listado={}
+  borrarTarea(id=''){
+    if(this._listado[id]){
+        delete this._listado[id];
     }
+  }
 
-    crearTarea(desc){
-        const tarea = new Tarea(desc)
-        this._listado[tarea.id] = tarea;
-    }
-
-    cargarTareasFromArray(tareas = []){
-        // Agrega las tareas al listado
-        tareas.forEach(tarea => {
-            this._listado[tarea.id] = tarea;
-        });
-    }
 
 }
 
